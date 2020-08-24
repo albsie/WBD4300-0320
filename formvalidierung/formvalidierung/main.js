12f0a474cf794385e95c970705f4417255e19d76
Alber Sieghard <albsieweb@gmail.com>
2020-08-24 18:30:32 +0200

    Formvalidierung - age

--- formvalidierung/formvalidierung/main.js
+++ formvalidierung/formvalidierung/main.js
@@ -7,13 +7,40 @@ form.addEventListener('submit', (Event)=>{
   Event.preventDefault();
   let errors = "";
   errBox.innerHTML = "";
+  // Name
   if(form.name.value.length < 2){
     errors += "<span>Der Name muss aus mindestens 2 Buchstaben bestehen</span>";
   }
-
+  // Email
+  if(validateEmail(form.email.value)){
+    form.email.style.color = "green";
+  }else{
+    form.email.style.color = "red";
+    errors += "<span>Diese Email Adresse ist falsch</span>";
+  }
+  // Passwort
+  if(form.password.value.length <= 8){
+    errors += "<span>Das Passwort muss aus mindestens 8 Zeichen bestehen</span>";
+  }
+// Passwortwiederholung
+if(form.password.value != form.passwordRpt.value){
+  errors += "<span>Die Passwortwiederholung stimmt nicht mit dem Passwort überein!</span>";
+}
+// Alter
+if(isNaN(Number(form.age.value))){
+errors += "<span>Dieses Feld darf nur aus Zahlen bestehen</span>";
+}
+if(form.age.value <=14 || form.age.value >= 100){
+  errors += "<span>Dieses Alter ist nicht erlaubt!</span>";
+}
   errBox.innerHTML = errors;
 });
 
+
+function validateEmail(email)
+{
+ return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+}
 
 /*
 function removeError(){
