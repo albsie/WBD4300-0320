2a42a9bd8bedc9211725b34f4d70e0ee405fbf5e
Alber Sieghard <albsieweb@gmail.com>
2020-08-25 18:27:28 +0200

    exercise finish

--- exercise/nestedMenu/js/main.js
+++ exercise/nestedMenu/js/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const box = document.querySelectorAll('.box'),
+      nestedBoxes = document.querySelectorAll('header>div>ul>li>ul');
+
+for(let i = 0; i < box.length; i++){
+  box[i].addEventListener('mouseover', ()=>{
+    for(let item of nestedBoxes){
+      item.classList.remove('toggleMenu');
+    }
+    nestedBoxes[i].classList.add('toggleMenu');
+  })
+}
--- exercise/toggleMenu/js/main.js
+++ exercise/toggleMenu/js/main.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const d = document,
+      menuBurger = d.getElementById('menuBurger'),
+      menuNav = d.querySelector('header nav'),
+      burgerDivs = d.querySelectorAll('#menuBurger div'),
+      menuItems = d.querySelectorAll('header nav ul li');
+
+      menuBurger.addEventListener('click', ()=>{
+        menuNav.classList.toggle('fadedOutMenu');
+        menuBurger.classList.toggle('rotate');
+        for(let item of burgerDivs){
+          item.classList.toggle('changeBackground');
+        }
+      });
+
+      for(let item of menuItems){
+        item.addEventListener('click', ()=>{
+          setTimeout(()=>{
+            menuNav.className = 'fadedOutMenu';
+            menuBurger.classList.toggle("rotate");
+            for(let item of burgerDivs){
+              item.classList.toggle('changeBackground');
+            }
+          },300);
+        })
+      }
